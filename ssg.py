89e88a9f251214c0e388daf6867b08ec58f7fa6f
Afaggian <95209107+Afaggian@users.noreply.github.com>
2021-12-16 20:58:01 +1000

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -4,8 +4,8 @@ import ssg.parsers
 from ssg.site import Site
 
 
-def main(source="content", dest="dist", parsers=[ssg.parsers.ResourceParser()]):
-    config = {"source": source, "dest": dest, "parsers": parsers}
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
 
     Site(**config).build()
 

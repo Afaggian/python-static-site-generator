baa05a22c0d7a18348b13ae94e98d8a91227af47
Afaggian <95209107+Afaggian@users.noreply.github.com>
2021-12-16 20:29:27 +1000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,7 +6,7 @@ class Parser:
     extensions: List[str] = []
 
     def valid_extension(self, extension):
-        return extension in self.extensions:
+        return extension in self.extensions
 
     def parse(self, path, source, dest):
         self.path = Path(path)
